c68b899097a54c3bff34eacf2825b2a60ec3e64e
Ben Lu <ben@ben-l.com>
2017-07-28 03:06:15 +1200

    Updated react and got 4 states of loading working

--- server/universal.js
+++ server/universal.js
@@ -12,12 +12,12 @@ const {default: App} = require('../src/containers/App')
 module.exports = function universalLoader(req, res) {
   const filePath = path.resolve(__dirname, '..', 'build', 'index.html')
 
-  fs.readFile(filePath, 'utf8', (err, htmlData)=>{
+  fs.readFile(filePath, 'utf8', async (err, htmlData)=>{
     if (err) {
       console.error('read err', err)
       return res.status(404).end()
     }
-    const context = {}
+    const context = {data: {}, head: [], req}
     const store = configureStore()
     const markup = renderToString(
       <Provider store={store}>
@@ -29,13 +29,41 @@ module.exports = function universalLoader(req, res) {
         </StaticRouter>
       </Provider>
     )
+    const keys = Object.keys(context.data)
+    const promises = keys.map(k=>context.data[k])
+    try {
+      const resolved = await Promise.all(promises)
+      resolved.forEach((r,i)=>context.data[keys[i]]=r)
+    } catch (err) {
+     // Render a better page than that? or just send the original markup, let the frontend handle it. Many options here
+      return res.status(400).json({message: "Uhhh, some thing didn't work"})
+    }
+    const newMarkup = renderToString(
+      <Provider store={store}>
+        <StaticRouter
+          location={req.url}
+          context={context}
+        >
+          <App/>
+        </StaticRouter>
+      </Provider>
+    )
+    const headMarkup = renderToString(
+      <div>
+        {context.head}
+      </div>
+    ).replace(/^<div.*?>/, '').replace(/<\/div>$/, '')
+    console.log('head', headMarkup)
+    console.log('data', context.data, JSON.stringify(context.data))
 
     if (context.url) {
       // Somewhere a `<Redirect>` was rendered
       redirect(301, context.url)
     } else {
       // we're good, send the response
-      const RenderedApp = htmlData.replace('{{SSR}}', markup)
+      const RenderedApp = htmlData.replace('{{SSR}}', newMarkup)
+        .replace('{{head}}', headMarkup)
+        .replace('{{DATA}}', JSON.stringify(context.data))
       res.send(RenderedApp)
     }
   })
--- src/containers/FirstPage.js
+++ src/containers/FirstPage.js
@@ -7,13 +7,47 @@ import { Link } from 'react-router-dom'
 import './FirstPage.css'
 
 class FirstPage extends Component {
+  async componentWillMount(){
+    this.state = {text: 'loading'}
+    
+    const {staticContext} = this.props
+    let loading = false
+    if (!staticContext || !staticContext.data.firstPage) {
+      if (!staticContext && window.DATA.firstPage) {
+        loading = window.DATA.firstPage
+      } else {
+        loading = this._getData()
+        console.log('inner loading', loading)
+      }
+      
+      if (staticContext) {
+        staticContext.data.firstPage = loading
+        return
+      }
+    } else if (staticContext){
+      const {text} = staticContext.data.firstPage
+      this.setState({text})
+      return
+    }
+    console.log('loading', loading)
+    const {text} = await loading
+    this.setState({text})
+  }
+  async _getData(){
+    const {text} = await new Promise(y=>setTimeout(()=>{
+      y({text: 'hi'}) // This is probably where you'd make an api call
+    },1000))
+    return {text}
+  }
   render() {
     const b64 = this.props.staticContext ? 'wait for it' : window.btoa('wait for it')
+    const {text} = this.state
     return (
       <div className='bold'>
         <h2>First Page</h2>
         <p>{`Email: ${this.props.user.email}`}</p>
         <p>{`b64: ${b64}`}</p>
+        <p>{`text: ${text}`}</p>
         <Link to={'/second'}>Second</Link>
       </div>
     )
