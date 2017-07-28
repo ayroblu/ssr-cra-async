686143bcb319152f2e3641176b9a86d4e4ad0695
Ben Lu <ben@ben-l.com>
2017-07-28 12:00:06 +1200

    Updated app page

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,10 +8,9 @@ export default class App extends Component {
   render(){
     return (
       <div>
-        <h1>Server Side Rendering with Create React App v2</h1>
-        <p>Hey, so I've rewritten this example with react-router v4</p>
-        <p>This code is on github: <a href='https://github.com/ayroblu/ssr-create-react-app-v2'>https://github.com/ayroblu/ssr-create-react-app-v2</a></p>
-        <p>Medium article: <a href='https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9'>https://medium.com/@benlu/ssr-with-create-react-app-v2-1b8b520681d9</a></p>
+        <h1>Server Side Rendering with Create React App - data loading</h1>
+        <p>This code is on github: <a href='https://github.com/ayroblu/ssr-cra-async'>https://github.com/ayroblu/ssr-cra-async</a></p>
+        <p>Medium article: <a href='https://gist.github.com/ayroblu/a6c4fcc1f397dcafe6a0ef9d46b66196'>https://gist.github.com/ayroblu/a6c4fcc1f397dcafe6a0ef9d46b66196</a></p>
         <Switch>
           <Route exact path="/" component={FirstPage}/>
           <Route path="/second" component={SecondPage}/>
